2abcd4a3e3ba0c2e727a653426e9204c8ae37910
Abrar Haque <abm.abrar@gmail.com>
2023-01-09 09:59:01 +0100

    added drag and drop functionality

--- js/view/Item.js
+++ js/view/Item.js
@@ -1,7 +1,10 @@
 import kanbanAPI from "../api/kanbanAPI.js";
+import DropZone from "./DropZone.js";
 
 export default class Item{
     constructor(id, content){
+        const bottomDropZone = DropZone.createDropZone();
+
         this.elements = {};
         this.elements.root = Item.createRoot();
         this.elements.input = this.elements.root.querySelector('.kanban__item-input');
@@ -9,6 +12,7 @@ export default class Item{
         this.elements.root.dataset.id = id;
         this.elements.input.textContent = content;
         this.content = content;
+        this.elements.root.appendChild(bottomDropZone);
 
         const onBlur = () => {
             const newContent = this.elements.input.textContent.trim();
@@ -33,6 +37,14 @@ export default class Item{
                 this.elements.input.removeEventListener('blur', onBlur);
                 this.elements.root.parentElement.removeChild(this.elements.root);
             }
+        });
+
+        this.elements.root.addEventListener('dragstart', (e)=>{
+            e.dataTransfer.setData('text/plain', id);
+        });
+
+        this.elements.input.addEventListener('drop', (e)=>{
+            e.preventDefault();
         });
     }
 
